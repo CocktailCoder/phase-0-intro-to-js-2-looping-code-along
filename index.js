b829836bf8b71c3ad3cce5862e77d1ceff0f19a0
cocktailcoder <iokreddit@protonmail.com>
2021-06-06 11:26:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+debugger;
+function writeCards(stringNames, eventName){
+    for(let i = 0; i < stringNames.length; i++){
+        let x = (`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`);
+        myNewArray.push(x);
+    }
+    return myNewArray;
+}
+const myNewArray = new Array;
+function countDown(number){
+    for (let i = number; i >= 0; i--){
+        console.log(i);
+    }
+
+}
+
+//console.log(countDown());
+//myNewArray();
